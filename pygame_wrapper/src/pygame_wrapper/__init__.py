205deb1725814256b019c41736cf96fac9e80c51
Duve3 <Duv3tabest@gmail.com>
2023-09-10 13:54:13 -0700

    0.1.2 - bug fix because i accidently exposed the entire pygame-ce code

--- pygame_wrapper/src/pygame_wrapper/__init__.py
+++ pygame_wrapper/src/pygame_wrapper/__init__.py
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: MIT
 from .game import GameType
 from .menu import MenuType
-from .colors import *
+from .colors import CustomColor
 from .fonts import Font
 # access to everything else should be done by import pygame-wrapper.blah.blah blah
